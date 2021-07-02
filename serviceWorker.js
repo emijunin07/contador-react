b7bfaf679023205b262a2cb32edb55ac8d84043d
emijunin07 <86810216+emijunin07@users.noreply.github.com>
2021-07-01 21:25:49 -0300

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,30 @@
+const CACHE_ELEMENTS = [
+    "./",
+    "https://unpkg.com/react@17/umd/react.production.min.js",
+    "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js",
+    "https://unpkg.com/@babel/standalone/babel.min.js",
+    "./estilo.css",
+    "./components/Contador.js",
+]
+
+const CACHE_NAME = "v1_cache_contador_react"
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            if (res) {
+                return res;
+            }
+
+            return fetch(e.request);
+        })
+    );
+});
+
+
+
+
+
+
+
+
